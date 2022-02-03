bc85bae5efc252167674321fb89b650104793171
Rutwik <rutwikhdev@gmail.com>
2022-02-03 19:50:45 +0530

    feat: Added doctype for settings

---
+++ storage_integration/storage_integration/doctype/storage_integration_settings/storage_integration_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Frappe Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Storage Integration Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
