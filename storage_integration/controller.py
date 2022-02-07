a761d1b600e8812a32c860371aaac76da0cd0609
Rutwik <rutwikhdev@gmail.com>
2022-02-07 21:04:02 +0530

    feat: Added ability to temporary clone and restore
    * temporary clone for taking backups
    * restore everything to previous state

--- storage_integration/controller.py
+++ storage_integration/controller.py
@@ -21,6 +21,10 @@ class MinioConnection:
 		)
 
 	def upload_file(self):
+		if re.search(r"\bhttps://\b", self.file.file_url):
+			# if file already on s3
+			return
+
 		key = self.get_object_key()
 		with open("./" + key, "rb") as f:
 			self.client.put_object(
@@ -38,16 +42,34 @@ class MinioConnection:
 		obj_key = self.get_object_key()
 		self.client.remove_object(self.settings.bucket_name, obj_key)
 
-	def download_file(self):
+	def download_file(self, action_type):
 		obj_key = self.get_object_key()
 
 		try:
 			response = self.client.get_object(
 				self.settings.bucket_name, obj_key, self.file.file_name
 			)
-			frappe.local.response["filename"] = self.file.file_name
-			frappe.local.response["filecontent"] = response.read()
-			frappe.local.response["type"] = "download"
+			if action_type == "download":
+				frappe.local.response["filename"] = self.file.file_name
+				frappe.local.response["filecontent"] = response.read()
+				frappe.local.response["type"] = "download"
+			elif action_type == "clone":
+				with open("./" + obj_key, "wb") as f:
+					f.write(response.read())
+			elif action_type == "restore":
+				with open("./" + obj_key, "wb") as f:
+					f.write(response.read())
+
+					if self.file.is_private:
+						pattern = frappe.local.site
+					else:
+						pattern = "/public"
+
+					match = re.search(rf"\b{pattern}\b", obj_key)
+					obj_key = obj_key[match.span()[1] :]
+					self.file.file_url = obj_key
+					self.file.save()
+
 			frappe.db.commit()
 		finally:
 			response.close()
@@ -82,7 +104,7 @@ def delete_from_s3(doc, method):
 def download_from_s3(doc_name):
 	doc = frappe.get_doc("File", doc_name)
 	conn = MinioConnection(doc)
-	conn.download_file()
+	conn.download_file(action_type="download")
 
 
 @frappe.whitelist()
@@ -95,9 +117,19 @@ def migrate_existing_files():
 
 
 @frappe.whitelist()
-def remove_all_data():
+def delete_all_remote():
 	files = frappe.get_all("File", pluck="name", filters={"file_url": ("!=", "")})
 
 	for file in files:
 		doc = frappe.get_doc("File", file)
 		delete_from_s3(doc, None)
+
+
+@frappe.whitelist()
+def clone_files(action_type):
+	files = frappe.get_all("File", pluck="name", filters={"file_url": ("!=", "")})
+
+	for file in files:
+		doc = frappe.get_doc("File", file)
+		conn = MinioConnection(doc)
+		conn.download_file(action_type=action_type)
