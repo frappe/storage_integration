dd577b8f5083d87ed5fa5dfffc032f5b7d489bc3
Rutwik <rutwikhdev@gmail.com>
2022-02-07 21:05:11 +0530

    feat: Added buttons and refactor

--- storage_integration/storage_integration/doctype/storage_integration_settings/storage_integration_settings.js
+++ storage_integration/storage_integration/doctype/storage_integration_settings/storage_integration_settings.js
@@ -3,32 +3,65 @@
 
 frappe.ui.form.on('Storage Integration Settings', {
 	refresh: function(frm) {
+		
+		createCustomButton(
+			frm,
+			"Migrate all to S3",
+			"All the local files will be delete and uploaded to AWS S3 storage.",
+			"storage_integration.controller.migrate_existing_files",
+		);
 
-		frm.add_custom_button(__("Migrate All"), () => {
-			frappe.confirm('All the local file attachments will be uploaded to AWS S3. Are you sure you want to proceed?',
-				() => {
-					// yes
-					frappe.call({
-						method: 'storage_integration.controller.migrate_existing_files',
-					})
-				}, () => {
-					// no
-				}
-			);
-		});
+		createCustomButton(
+			frm,
+			"Delete all from S3",
+			"All the remote files will be deleted from S3 storage, make sure you have cloned the files for backup or restored to the local storage.",
+			"storage_integration.controller.delete_all_remote",
+		);
 
-		frm.add_custom_button(__("Remove All"), () => {
-			frappe.confirm('All the remote(S3) attachments will be deleted, make sure you have taken the backups. Are you sure you want to proceed?',
-				() => {
-					// yes
-					frappe.call({
-						method: 'storage_integration.controller.remove_all_data',
-					})
-				}, () => {
-					// no
-				}
-			);
-		});
+		createCustomButtonWithArgs(
+			frm,
+			"Clone for backup",
+			"All the remote(S3) files will be downloaded to your site, this operation will not delete the remote files.",
+			"storage_integration.controller.clone_files",
+			"clone"
+		);
+
+		createCustomButtonWithArgs(
+			frm,
+			"Restore to local",
+			"All the remote files will be restored locally.",
+			"storage_integration.controller.clone_files",
+			"restore"
+		)
 
 	}
 });
+
+function createCustomButton(frm, title, description, method) {
+	return frm.add_custom_button(__(title), () => {
+			frappe.confirm(description,
+				() => {
+					// yes
+					frappe.call({
+						method: method,
+					})
+				}
+			);
+		}, __("Actions"));
+}
+
+function createCustomButtonWithArgs(frm, title, description, method, action) {
+	return frm.add_custom_button(__(title), () => {
+			frappe.confirm(description,
+				() => {
+					// yes
+					frappe.call({
+						method: method,
+						args: {
+							action_type: 'restore'
+						}
+					})
+				}
+			);
+		}, __("Actions"));
+}
